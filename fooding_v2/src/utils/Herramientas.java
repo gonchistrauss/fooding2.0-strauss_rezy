9f29cced5346af4720aad24774c996f855932adb
Gonzalo Strauss <gostrauss8@hotmail.com>
2017-11-18 10:38:05 -0300

    Se agrega clase Herramientas en utils

---
+++ fooding_v2/src/utils/Herramientas.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.util.Locale;
+
+public class Herramientas {
+
+    public static String[] obtenerpaises() {
+        String[] countries = new String[Locale.getISOCountries().length];
+        String[] countryCodes = Locale.getISOCountries();
+        for (int i = 0; i < countryCodes.length; i++) {
+            Locale obj = new Locale("", countryCodes[i]);
+            countries[i] = obj.getDisplayCountry();
+        }
+        return countries;
+
+    }
+
+}
